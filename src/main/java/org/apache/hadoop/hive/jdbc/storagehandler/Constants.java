182aad8d8da084beeff70d85a6d32a3d83b4afa9
Divyanshu Goyal <divyanshug@qubole.com>
2015-07-10 14:24:27 +0530

    LICENSE Added

--- src/main/java/org/apache/hadoop/hive/jdbc/storagehandler/Constants.java
+++ src/main/java/org/apache/hadoop/hive/jdbc/storagehandler/Constants.java
@@ -19,7 +19,7 @@ package org.apache.hadoop.hive.jdbc.storagehandler;
 
 public final class Constants {
 
-	public static final String LIST_COLUMNS = "columns";
+    public static final String LIST_COLUMNS = "columns";
     public static final String LIST_COLUMN_TYPES = "columns.types";
 
     private Constants() {
