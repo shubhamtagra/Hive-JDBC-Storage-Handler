6920163615fbf1152ef8f753a299ad4a30425a28
Shubham Tagra <stagra@qubole.com>
2015-08-17 13:18:04 +0530

    Cancel statement while closing

--- src/main/java/org/apache/hadoop/hive/jdbc/storagehandler/GenericDBRecordReader.java
+++ src/main/java/org/apache/hadoop/hive/jdbc/storagehandler/GenericDBRecordReader.java
@@ -1,11 +1,14 @@
 package org.apache.hadoop.hive.jdbc.storagehandler;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
 import org.apache.hadoop.mapreduce.lib.db.DBInputFormat;
 import org.apache.hadoop.mapreduce.lib.db.DBRecordReader;
 import org.apache.hadoop.mapreduce.lib.db.DBWritable;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,10 +20,14 @@ import static org.apache.hadoop.hive.jdbc.storagehandler.Constants.INPUT_FETCH_S
  */
 public class GenericDBRecordReader<T extends DBWritable> extends DBRecordReader<T>
 {
+    final Configuration conf;
+    private static final Log LOG = LogFactory.getLog(GenericDBRecordReader.class);
+
     public GenericDBRecordReader(DBInputFormat.DBInputSplit split, Class<T> inputClass, Configuration conf, Connection conn, DBConfiguration dbConfig, String cond, String[] fields, String table)
             throws SQLException
     {
         super(split, inputClass, conf, conn, dbConfig, cond, fields, table);
+        this.conf = conf;
     }
 
     protected ResultSet executeQuery(String query) throws SQLException {
@@ -28,5 +35,18 @@ public class GenericDBRecordReader<T extends DBWritable> extends DBRecordReader<
                 ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
         statement.setFetchSize(conf.getInt(INPUT_FETCH_SIZE, DEFAULT_INPUT_FETCH_SIZE));
         return statement.executeQuery();
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        try {
+            statement.cancel();
+        }
+        catch (SQLException e) {
+            // Ignore any errors in cancelling, this is not fatal
+            LOG.error("Could not cancel query: "  + this.getSelectQuery());
+        }
+        super.close();
     }
 }
